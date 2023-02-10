50069efe2c0e98542c5f6392017e5830cddfa5b5
SASANKA KUNDU <94476373+sasanka0212@users.noreply.github.com>
2023-02-10 23:51:04 +0530

    Create Question_09.java

---
+++ Question_09.java
@@ -0,0 +1,23 @@
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+public class Question_09
+{
+	public static void main(String args[]) {
+		try 
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sasa", "root", "2002");
+			CallableStatement cst = connection.prepareCall("select count(*) from student");
+			ResultSet rs = cst.executeQuery();
+			while(rs.next()) {
+				System.out.print("number of rows : " + rs.getInt(1));
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
