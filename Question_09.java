7ec6a24beed9356881395133d5a7bb370070db5c
SASANKA KUNDU <94476373+sasanka0212@users.noreply.github.com>
2023-02-10 23:52:07 +0530

    Update Question_09.java

--- Question_09.java
+++ Question_09.java
@@ -8,8 +8,11 @@ public class Question_09
 	public static void main(String args[]) {
 		try 
 		{
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sasa", "root", "2002");
+			//Class.forName("com.mysql.cj.jdbc.Driver");
+			//Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sasa", "root", "2002");
+			
+			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());	//driver register
+			Connection connection=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","system","sasa");	
 			CallableStatement cst = connection.prepareCall("select count(*) from student");
 			ResultSet rs = cst.executeQuery();
 			while(rs.next()) {
